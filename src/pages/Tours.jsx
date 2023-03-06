e3bf4b34ca4b49070d397b9acacb854229f87566
juliosc1 <julio.siklander@gmail.com>
2023-03-06 19:15:07 +0100

    add grid setup for all image in tours page

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import LasPalmasImg from '../assets/laspalmas.jpg'
+import VenedigImg from '../assets/venedig.jpg'
+import CyprusImg from '../assets/cyprus.jpg'
+import NewYorkImg from '../assets/newyork.jpg'
+import NorwayImg from '../assets/norway.jpg'
 
 const Tours = () => {
   return (
@@ -7,12 +12,12 @@ const Tours = () => {
 
         <div className='border-2'></div>
         
-        <div className='grid grid-cols-5 grid-rows-5 border-2 gap-2 p-20'>
-          <div className='row-span-3 col-span-2 border'>1</div>
-          <div className='row-span-2 col-span-3 border'>2</div>
-          <div className='col-span-3 border'>3</div>
-          <div className='row-span-2 col-span-3 border'>4</div>
-          <div className='row-span-2 col-span-2 border'>5</div>
+        <div className='grid grid-cols-5 grid-rows-5 sm:grid-cols-3 md:grid-cols-5 border-2 gap-2 p-10 lg:p-20'>
+          <div className='row-span-3 col-span-2 sm:row-span-2 sm:col-span-3 md:row-span-3 md:col-span-2 border rounded-xl' style={{backgroundImage: `url(${LasPalmasImg})`, backgroundRepeat: "no-repeat" , backgroundSize:"cover"}}></div>
+          <div className='row-span-2 col-span-3 border rounded-xl' style={{backgroundImage: `url(${VenedigImg})`, backgroundRepeat: "no-repeat" , backgroundSize:"cover"}}></div>
+          <div className='col-span-3 sm:row-span-2 sm:col-span-3 md:row-span-1 border rounded-xl' style={{backgroundImage: `url(${CyprusImg})`, backgroundRepeat: "no-repeat" , backgroundSize:"cover"}}></div>
+          <div className='row-span-2 col-span-3 sm:hidden md:flex md:col-span-5 lg:col-span-3 border rounded-xl' style={{backgroundImage: `url(${NewYorkImg})`, backgroundRepeat: "no-repeat" , backgroundSize:"cover"}}></div>
+          <div className='row-span-2 col-span-2 sm:hidden border rounded-xl lg:flex ' style={{backgroundImage: `url(${NorwayImg})`, backgroundRepeat: "no-repeat" , backgroundSize:"cover"}}></div>
         </div>
       
       </div>
